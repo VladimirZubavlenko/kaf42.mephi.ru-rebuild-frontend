4e692d498395674c474ada49af92522a99aeafde
vladimirzubavlenko <vladimirzubavlenko@icloud.com>
2021-04-13 13:30:28 +0300

    [fix] some translations

--- src/pages/Feedback.tsx
+++ src/pages/Feedback.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
+import { useIntl } from 'react-intl';
 import { LayoutContent } from '../layout/LayoutContent';
 import { Feedback as FeedbackComponent } from '../feedback/Feedback';
 
 const Feedback: React.FC = () => {
+  const locale = useIntl();
+
   React.useEffect(() => {
     document.title = 'Обратная связь | Кафедра 42';
   });
 
   return (
-    <LayoutContent title="Обратная связь">
+    <LayoutContent title={locale.messages['contacts.title'].toLocaleString()}>
       <FeedbackComponent />
     </LayoutContent>
   );
